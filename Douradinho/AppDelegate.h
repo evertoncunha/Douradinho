e0e55e75785cec5a218c67bbdf46fbc108ee6f54
Everton <evertonmaverick@MacBook-de-Everton.local>
2013-09-02 16:54:19 -0300

    Initial Commit

---
+++ Douradinho/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Douradinho
+//
+//  Created by Everton on 02/09/13.
+//  Copyright (c) 2013 Everton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DouradinhoTests/DouradinhoTests.m
@@ -0,0 +1,34 @@
+//
+//  DouradinhoTests.m
+//  DouradinhoTests
+//
+//  Created by Everton on 02/09/13.
+//  Copyright (c) 2013 Everton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DouradinhoTests : XCTestCase
+
+@end
+
+@implementation DouradinhoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
